24b63fd046031bbd1efa34a813b84ad247e48067
tti0 <14841064+tti0@users.noreply.github.com>
2022-05-04 17:56:44 +0100

    Tidy documentation for functions written so far

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 # gsheets-getter-functions
 
-Please do not use these functions yet.
+Functions to load data from the MGS Sports Day spreadsheet
+
+*NOT FOR PUBLIC RELEASE*
+
+## API Keys
+
+Public Google Sheets API key (read-only): `AIzaSyCFfbIjKZGPkuXnYUFD4E14flZNKMC9rQE`
+
+Public Google Sheet ID for 2022: `1l5ZpGQ6ElmXMdLbr801MFv8cOBf9QVfAHJqiCQti1q0`
+
+## List of functions
+
+All but the helper functions use async/await, and depend on Underscore.js and Axios (see `package.json`).
+
+All functions are documented with jsDoc in `index.js`, but have not been exported. Simply copy-paste into the new SD app, making modifications (TS, Fetch API, etc.) as you see fit.
+
+Returned data is sorted in spreadsheet order.
+
+These functions need updating year-on-year, as the spreadsheet cell references are likely to change.
+
+Please do not hesitate to ask if anything needs clarifying.
+
+*Struck-through functions have not been implemented yet.*
+
+### Helper functions needed by others
+
++ sdBuildQuery
++ sdParseRes
++ intToSpreadsheetColLetter
+
+### Functions to be run once on app load, and response stored
+
++ getEventsList
++ getFormsList
++ getBonusPointsAllocations
+
+### Functions to be run more than once (i.e. to refresh data or when user requests specific event)
+
++ getSummaryStandings
++ getEventResults
++ ~~getEventRecord~~
++ ~~getFormResults~~
++ ~~getRecordStatistics~~
++ ~~getYearGroupRecords~~
+
+## Licensing
+
+These functions are Copyright (c) 2017-22 Pal Kerecsenyi and Theodore Tucker, and are licensed under the MIT License (see `LICENSE`).
+
+This repository is not intended for public distribution.
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// DO NOT USE THESE FUNCTIONS YET
+// gsheets-getter-functions
+// Functions to load data from the MGS Sports Day spreadsheet
+//
+// (c) 2017-22 PPK, TGT; MIT License
+// This repository is not intended for public distribution.
 
 var axios = require("axios").default;
 var _ = require("underscore");
@@ -111,6 +115,22 @@ async function getFormsList(apiKey, sheetId) {
   }
 }
 
+/**
+ * Get the table of bonus points awarded when records are broken/equalled from the spreadsheet
+ * Useful to run this function once on app load and store results locally
+ *  as only bonus point values (not their meanings) are retrieved for each event.
+ *  Otherwise, there is no way to verbally indicate to the user if a record has been broken.
+ * @async
+ * @param {string} apiKey - Google Sheets API key
+ * @param {string} sheetId - The ID of the public Google Sheet to query
+ * @returns {Promise<Object>} - See example below
+ * Example: 
+ * {
+ *    noRecord: 0,
+ *    equal: 1,
+ *    beat: 2
+ * }
+ */
 async function getBonusPointsAllocations(apiKey, sheetId) {
   var queryURL = sdBuildQuery(apiKey, sheetId, "point_allocations_record!B3:B5", "ROWS", false);
   try {
@@ -126,6 +146,20 @@ async function getBonusPointsAllocations(apiKey, sheetId) {
   }
 }
 
+/**
+ * Get a list of all forms, their total points, and year group and whole school position standings
+ * @async
+ * @param {string} apiKey - Google Sheets API key
+ * @param {string} sheetId - The ID of the public Google Sheet to query
+ * @returns {Promise<Array>} - See example below
+ * Example: 
+ * [
+      { year: 7, form: 'B', points: 480, yearPos: 2, schoolPos: 9 },
+      { year: 7, form: 'D', points: 424, yearPos: 7, schoolPos: 24 },
+      { year: 7, form: 'E', points: 480, yearPos: 2, schoolPos: 9 },
+      { year: 7, form: 'H', points: 449, yearPos: 6, schoolPos: 20 }, ...
+ * ]
+ */
 async function getSummaryStandings(apiKey, sheetId) {
   var queryURL = sdBuildQuery(apiKey, sheetId, "summary!A3:E37", "ROWS", false);
   try {
@@ -137,10 +171,46 @@ async function getSummaryStandings(apiKey, sheetId) {
   }
 }
 
+
+/**
+ * For a single event and year group, get the positions and points scored by competitors from each form, in each of the sub-events A, B, C
+ *  Also returns total points per form in that event, and number of Record Bonus points
+ * @async
+ * @param {string} apiKey - Google Sheets API key
+ * @param {string} sheetId - The ID of the public Google Sheet to query
+ * @param {Array} eventsList - The array of events returned by getEventsList()
+ * @param {Array} formsList - The array of forms returned by getFormsList()
+ * @param {String} eventDbName - The database name (the "db" field in eventsList) of the event for which results are to be retrieved
+ * @param {Number} yearGroup - The year group for which results are to be retrieved (one of [7, 8, 9, 10])
+ * @returns {Promise<Object>} - See example below
+ * Example: 
+ * {
+  A: [
+    { letter: 'AC/GMT', pos: 8, pts: 23 },
+    { letter: 'DVN/JPJ', pos: 1, pts: 31 }, ...
+  ],
+  B: [
+    { letter: 'AC/GMT', pos: 5, pts: 16 },
+    { letter: 'DVN/JPJ', pos: 6, pts: 15 }, ...
+  ],
+  C: [
+    { letter: 'AC/GMT', pos: 6, pts: 5 },
+    { letter: 'DVN/JPJ', pos: 8, pts: 3 }, ...
+  ],
+  RB: [
+    { letter: 'AC/GMT', pos: '', pts: 0 },
+    { letter: 'DVN/JPJ', pos: '', pts: 2 }, ...
+  ],
+  TOTAL: [
+    { letter: 'AC/GMT', pos: '', pts: 44 },
+    { letter: 'DVN/JPJ', pos: '', pts: 51 }, ...
+  ]
+}
+ */
 async function getEventResults(apiKey, sheetId, eventsList, formsList, eventDbName, yearGroup) {
   var matchingEvent = _.where(eventsList, {db: eventDbName})[0];
   var matchingYearLetters = _.where(formsList, {year: parseInt(yearGroup)});
-  var spreadsheetRange = "y" + yearGroup + "_results!" + intToSpreadsheetColLetter(matchingEvent.startingCol) + "9:" +  intToSpreadsheetColLetter(matchingEvent.startingCol + 4) + "24";
+  var spreadsheetRange = "y" + yearGroup + "_results!" + intToSpreadsheetColLetter(matchingEvent.startingCol) + "9:" +  intToSpreadsheetColLetter(matchingEvent.startingCol + 4) + (yearGroup == "9" ? "28" : "24"); // year 9 has 10 forms this year; all other have 8 forms
   var queryURL = sdBuildQuery(apiKey, sheetId, spreadsheetRange, "COLUMNS", false);
   try {
     var response = await axios.get(queryURL);
@@ -194,19 +264,22 @@ async function getEventResults(apiKey, sheetId, eventsList, formsList, eventDbNa
   }
 }
 
+
+// END OF FUNCTIONS
+
+// TGT testing code
 (async() => {
   var events = await getEventsList("AIzaSyCFfbIjKZGPkuXnYUFD4E14flZNKMC9rQE", "1l5ZpGQ6ElmXMdLbr801MFv8cOBf9QVfAHJqiCQti1q0");
   var forms = await getFormsList("AIzaSyCFfbIjKZGPkuXnYUFD4E14flZNKMC9rQE", "1l5ZpGQ6ElmXMdLbr801MFv8cOBf9QVfAHJqiCQti1q0");
-  console.log(forms);
-  console.log(await getEventResults("AIzaSyCFfbIjKZGPkuXnYUFD4E14flZNKMC9rQE", "1l5ZpGQ6ElmXMdLbr801MFv8cOBf9QVfAHJqiCQti1q0", events, forms, "100m", 10));
+  console.log(await getEventResults("AIzaSyCFfbIjKZGPkuXnYUFD4E14flZNKMC9rQE", "1l5ZpGQ6ElmXMdLbr801MFv8cOBf9QVfAHJqiCQti1q0", events, forms, "100m", 9));
 })()
 
 /*
 
 TODO
 
-event_record(id, year) = l. 389
-form(year, form) - l. 431
+event_record(id, year) = SD1 l. 389
+form(year, form) - SD1 l. 431
 records summary = records_summary!A3:C8
 records(year)
 
